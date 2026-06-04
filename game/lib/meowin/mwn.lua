09704063a31f2c1ea1da6f3815d5983d0b246992
Meowybot <jorgedidier149@gmail.com>
2026-06-04 13:11:47 -0600

    Update mwn.lua

--- game/lib/meowin/mwn.lua
+++ game/lib/meowin/mwn.lua
@@ -1,7 +1,22 @@
 local mwn = {}
 
-function mwn.toTable(str)
+function mwn.toTable(name)
   local t = {}
+  local currsec = nil
+  local nests = {}
+  local nestn = {}
+  local nesta = #nests
+  for line in love.filesystem.lines(name) do
+    if not currsec then
+      --[[
+        make the section be the name if starts w # and ends w " {"
+      --]]
+    else
+      --[[
+        many things very code
+      --]]
+    end
+  end
 end
 
 return mwn
