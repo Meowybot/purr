b677d7d40822e37e6743d3cd1c17d6362e758d1f
Meowybot <jorgedidier149@gmail.com>
2026-06-05 11:12:41 -0600

    fix currsec and add error

--- game/lib/meowin/mwn.lua
+++ game/lib/meowin/mwn.lua
@@ -2,23 +2,38 @@ local mwn = {}
 
 function mwn.toTable(name)
   local t = {}
+  local currlinen = 0
   local currsec = nil
   local nests = {}
   local nestn = {}
   local nesta = #nests
   for lineu in love.filesystem.lines(name) do
+    currlinen = currlinen + 1
     local line = string.gsub(lineu, "^%s+", "")
     local firstchar = string.sub(line,1,1)
+    local secndchar = string.sub(line,2,2)
     local lastchar = string.sub(line,-1,-1)
     if not currsec then
       if firstchar == "#" then
-        t[line:sub(2, -3)] = t[line:sub(2, -3)] or {}
+        currsec = line:sub(2,-3)
+        t[currsec] = t[currsec] or {}
       end
     else
+      if firstchar == "}" then
+        if nesta == 0 then
+          currsec = nil
+        else
+          error("Meowin' file " .. name .. ", line " .. currlinen .. ": Did not close other " .. nesta .. " lists/arrays before closing section")
+        end
+      else
+        
+      end
       --[[
         okay so basically do:
         if its a regular value put it as normal index
         if its starts with * or $ do a list or numbered list
+        if its ] or } or ) end the current
+        if its // ignore it
         thats it
       --]]
     end
