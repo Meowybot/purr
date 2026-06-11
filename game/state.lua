3dd5fa5a59b4b74dca7d324c25c650f15a150a20
Meowybot <jorgedidier149@gmail.com>
2026-06-11 16:38:51 -0600

    Update state.lua

--- game/state.lua
+++ game/state.lua
@@ -1,3 +1,14 @@
 local state = {}
 
+state.load = function() end
+state.update = function() end
+state.draw = function() end
+state.keypressed = function() end
+state.keyreleased = function() end
+state.mousepressed = function() end
+state.mousereleased = function() end
+
+function state.switch(newstate, ...)
+end
+
 return state
