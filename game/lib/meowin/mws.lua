a41286d96bcd6f351311f2ec73cc3cdd1b183155
Meowybot <jorgedidier149@gmail.com>
2026-06-21 11:28:20 -0600

    Update mws.lua

--- game/lib/meowin/mws.lua
+++ game/lib/meowin/mws.lua
@@ -2,8 +2,15 @@ local mwn = require("lib.meowin.mwn")
 
 local mws = {}
 
-function mws.toLua(t)
-  t
+function mws.toLua(name)
+  local t
+  if type(name) == "string" then
+    t = mwn.toTable(name)
+  elseif type(name) == "table" then
+    t = name
+  else
+    error("place")
+  end
 end
 
 return mws
