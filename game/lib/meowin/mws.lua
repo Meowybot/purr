11a0a85dc12aa07c454952506ceae39efe5d7989
Meowybot <jorgedidier149@gmail.com>
2026-06-21 13:25:28 -0600

    Update mws.lua

--- game/lib/meowin/mws.lua
+++ game/lib/meowin/mws.lua
@@ -2,7 +2,9 @@ local mwn = require("lib.meowin.mwn")
 
 local mws = {}
 
-function mws.toLua(name)
+function mws.toLua(name, funcTrans, udft, cbTrans, udct)
+  local luas = "local mwztr = {}\n"
+  local luend = "return mwztr\n"
   local t
   if type(name) == "string" then
     t = mwn.toTable(name)
@@ -11,6 +13,16 @@ function mws.toLua(name)
   else
     error("place")
   end
+  local dft = udft ~= false
+  local dct = udct ~= false
+  for i, v in pairs(t) do
+
+    if cbTrans[i] then
+    elseif dct[i] then
+    end
+
+  end
+  luas = luas .. luend
 end
 
 return mws
